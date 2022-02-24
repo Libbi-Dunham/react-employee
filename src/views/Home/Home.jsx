5627400fa1cb4e060f32b99125e1ed2e36983a6d
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-24 11:32:23 -0800

    wrote out my home w/ working links and header

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
 export default function Home() {
-  return <div>Home</div>;
+  const { user } = useUser();
+  return (
+    <>
+      <h1>Welcome to Acem Inc.</h1>
+      <p>As part of joining Acem please choose an option below</p>
+      <p>Thank you for being apart of our team</p>
+      {user?.email ? (
+        <Link to="/profile">Take a look at your Profile</Link>
+      ) : (
+        <>
+          <Link to="/register">Make an Account</Link>
+          {' or '}
+          <Link to="/login">Sign in</Link>
+        </>
+      )}
+    </>
+  );
 }
