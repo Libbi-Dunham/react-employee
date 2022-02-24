d459695e505802d20906002aa7bc7b9c3bc41b6d
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-24 10:32:57 -0800

    wrote out userContext and apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import Home from './views/Home/Home';
+import Auth from './views/Auth/Auth';
+import ConfirmEmail from './views/Auth/ConfirmEmail';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Header from './components/Layout/Header';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Auth />
+          </Route>
+          <Route path="/register">
+            <Auth />
+          </Route>
+          <Route path="/confirm-email">
+            <ConfirmEmail />
+          </Route>
+          <PrivateRoute path="/profile"></PrivateRoute>
+        </Switch>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
