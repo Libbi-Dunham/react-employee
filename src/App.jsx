046adb5f52e8cee266b2761d92e49278814f502e
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-24 11:01:39 -0800

    wrote out useForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
+// import { UserProvider } from './context/UserContext';
 import Home from './views/Home/Home';
 import Auth from './views/Auth/Auth';
 import ConfirmEmail from './views/Auth/ConfirmEmail';
@@ -9,25 +9,25 @@ import Header from './components/Layout/Header';
 
 export default function App() {
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Auth />
-          </Route>
-          <Route path="/register">
-            <Auth />
-          </Route>
-          <Route path="/confirm-email">
-            <ConfirmEmail />
-          </Route>
-          <PrivateRoute path="/profile"></PrivateRoute>
-        </Switch>
-      </BrowserRouter>
-    </UserProvider>
+    // <UserProvider>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Auth />
+        </Route>
+        <Route path="/register">
+          <Auth />
+        </Route>
+        <Route path="/confirm-email">
+          <ConfirmEmail />
+        </Route>
+        <PrivateRoute path="/profile"></PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+    // </UserProvider>
   );
 }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,24 +1,24 @@
-import { createContext, useContext, useState } from 'react';
-import { getUser } from '../services/users';
+// import { createContext, useContext, useState } from 'react';
+// import { getUser } from '../services/users';
 
-export const UserContext = createContext();
+// export const UserContext = createContext();
 
-const UserProvider = ({ children }) => {
-  const currentUser = getUser();
-  const [user, setUser] = useState(
-    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
-  );
+// const UserProvider = ({ children }) => {
+//   const currentUser = getUser();
+//   const [user, setUser] = useState(
+//     currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+//   );
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
 
-const useUser = () => {
-  const context = useContext(UserContext);
+// const useUser = () => {
+//   const context = useContext(UserContext);
 
-  if (context === undefined) {
-    throw new Error('has to be within a UserProvider');
-  }
-  return context;
-};
+//   if (context === undefined) {
+//     throw new Error('has to be within a UserProvider');
+//   }
+//   return context;
+// };
 
-export { UserProvider, useUser };
+// export { UserProvider, useUser };
