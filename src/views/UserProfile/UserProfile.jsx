74d996f06214c572b72de4f1f9813b9bf2404463
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-24 17:43:35 -0800

    wrote out profile form

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 export default function UserProfile() {
   return (
     <>
-      <p>User profile</p>
+      <p>You are now in your profile!</p>
+      <p>If you do not have a profile click the option to create one</p>
+      <p>If you already have a profile click the option to edit</p>
       <Link to="/profile/edit">Edit Profile</Link>
+      <Link to="/profile/edit">Create Profile</Link>
     </>
   );
 }
